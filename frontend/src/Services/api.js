5318c5675f2fdf4202083c0cffd5cc29d057f99b
simone petta <simo.petta2@gmail.com>
2026-05-13 20:21:53 +0200

    aggiunto env

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const apiRequest = async (endpoint, options = {}) => {
     const headers = { ...options.headers };
