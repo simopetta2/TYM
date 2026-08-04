4a5d6714cd20b28935ba0d558bd638356e2e51b8
simone petta <simo.petta2@gmail.com>
2026-08-04 15:08:00 +0200

    cambio valori db

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,19 @@
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
-
 dotenv.config()
 
 export async function connect() {
+    if (!process.env.MONGODB_URI) {
+        console.error('CRITICO: MONGODB_URI non è definita nelle variabili d\'ambiente.')
+        process.exit(1)
+    }
+
     try {
         await mongoose.connect(process.env.MONGODB_URI)
-        console.log('connesso al database db')
-    }
-    catch (err) {
-        console.log('errore nella connessione al database db', err)
+        console.log('Connessione al database riuscita')
+    } catch (err) {
+        console.error('Errore nella connessione al database:', err)
+        process.exit(1)
     }
 }
